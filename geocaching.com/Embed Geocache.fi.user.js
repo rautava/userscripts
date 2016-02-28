07b61a295aba0a88b7937c7f4e75f3d7a3338d8f
Tommi Rautava <tommi.rautava@gmail.com>
2016-02-28 11:25:04 +0200

    Adapts to GC.com https URLs
    Since geocaching.com started using https URLs, iframe is replaced with
    HTTP request and HTML injection.

--- geocaching.com/Embed Geocache.fi.user.js
+++ geocaching.com/Embed Geocache.fi.user.js
@@ -1,19 +1,21 @@
 // ==UserScript==
 // @id             embedgeocachefi@kenmooda@gmail.com
 // @name           Embed Geocache.fi
-// @version        1.0.3
+// @version        1.1
 // @namespace      assankassa@gmail.com
 // @author         Tommi Rautava
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @description    Embed cache info frame from Geocache.fi on a Geocaching.com cache page.
-// @include        http://www.geocaching.com/seek/cache_details.aspx*
-// @include        http://www.geocaching.com/geocache/*
+// @updateURL      https://greasyfork.org/scripts/5244-embed-geocache-fi/code/Embed%20Geocachefi.user.js
+// @include        https://www.geocaching.com/seek/cache_details.aspx*
+// @include        https://www.geocaching.com/geocache/*
+// @grant          GM_xmlhttpRequest
 // @run-at         document-end
 // ==/UserScript==
 ////////////////////////////////////////////////////////////////////////////////
 //
 //    Embed Geocache.fi
-//    Copyright (C) 2011, 2013  Tommi Rautava
+//    Copyright (C) 2011, 2013, 2016  Tommi Rautava
 //
 //    This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
@@ -29,44 +31,39 @@
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 ////////////////////////////////////////////////////////////////////////////////
-
 var gcCodeElem = document.getElementById('ctl00_ContentBody_CoordInfoLinkControl1_uxCoordInfoCode');
 
 if (gcCodeElem) {
-	var gcCode = gcCodeElem.textContent;
-	
-	if (gcCode) {
-		var downloadDivElem = document.getElementById('Download');
-		
-		if (downloadDivElem) {
-			var refNode = document.evaluate("//div[@class='InformationWidget Clear']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    var gcCode = gcCodeElem.textContent;
 
-			if (refNode) {
-				var gcfiFrame = document.createElement("iframe");
-				gcfiFrame.src = 'http://www.geocache.fi/caches/cachetieto.php?printpage=1&wp='+ gcCode;
-				gcfiFrame.setAttribute('width', '100%');
-				gcfiFrame.setAttribute('height', '205px');
-				gcfiFrame.setAttribute('style', 'border:1px solid #b0b0b0;padding-bottom:1px');
-				refNode.parentNode.insertBefore(gcfiFrame, refNode.parentNode.firstChild);
-			}
-			else
-			{
-				GM_log("Ref node not found.");
-			}
-		}
-		else
-		{
-			GM_log("Download node not found.");
-		}
-	}
-	else
-	{
-		GM_log("GC code not found.");
-	}
-}
-else
-{
-	GM_log("GC code node not found.");
+    if (gcCode) {
+        var downloadDivElem = document.getElementById('Download');
+
+        if (downloadDivElem) {
+            var refNode = document.evaluate("//div[@class='InformationWidget Clear']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+            if (refNode) {
+                var gcfiFrame = document.createElement("div");
+                gcfiFrame.setAttribute('style', 'width:100%;max-height:200px;border:1px solid #b0b0b0;padding-bottom:1px;overflow-y:scroll;');
+                refNode.parentNode.insertBefore(gcfiFrame, refNode.parentNode.firstChild);
+                GM_xmlhttpRequest({
+                    method: "GET",
+                    url: "http://www.geocache.fi/caches/cachetieto.php?printpage=1&wp=" + gcCode,
+                    onload: function(res) {
+                        gcfiFrame.innerHTML = res.responseText.replace(/img src="\//g, 'img src="http://www.geocache.fi/');
+                    }
+                });
+            } else {
+                GM_log("Ref node not found.");
+            }
+        } else {
+            GM_log("Download node not found.");
+        }
+    } else {
+        GM_log("GC code not found.");
+    }
+} else {
+    GM_log("GC code node not found.");
 }
 
 // EOF
