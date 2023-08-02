e27f260a6d3847ebeab3d0c3f08335deeee85f34
Tommi Rautava <rautava@users.noreply.github.com>
2023-08-02 15:47:27 +0300

    Add Auto Expand script

---
+++ geni.com/Auto Expand.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Auto Expand
+// @namespace    4525639+rautava@users.noreply.github.com
+// @version      1.0
+// @description  Expand Bio sections automatically.
+// @author       Tommi Rautava
+// @license      CC0 1.0 Universal
+// @match        https://www.geni.com/people/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  var readMoreElem = document.getElementById("bio_en-US_toggle");
+
+  if (readMoreElem) {
+    readMoreElem.click();
+  }
+})();
