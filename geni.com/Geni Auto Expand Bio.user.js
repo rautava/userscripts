4d3aa21bb6c0e0372f7258f7122e018b31a38585
Tommi Rautava <4525639+rautava@users.noreply.github.com>
2023-10-15 21:45:14 +0300

    Update Geni Auto Expand Bio.user.js
    Expand the list of children.

--- geni.com/Geni Auto Expand Bio.user.js
+++ geni.com/Geni Auto Expand Bio.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Geni Auto Expand Bio
 // @namespace    4525639+rautava@users.noreply.github.com
-// @version      1.1.1
+// @version      1.2
 // @description  Expand Bio sections automatically.
 // @author       Tommi Rautava
 // @license      CC0-1.0
@@ -12,9 +12,15 @@
 (function () {
   "use strict";
 
-  var readMoreElem = document.querySelector('a.toggle_link[id^="bio_"]');
+  let readMoreElem = document.querySelector('a.toggle_link[id^="bio_"]');
 
   if (readMoreElem) {
     readMoreElem.click();
+  }
+
+  let moreChildrenElem = document.evaluate('//tr[@id="family_handprint"]//a[@href="#"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+
+  if (moreChildrenElem.singleNodeValue) {
+    moreChildrenElem.singleNodeValue.click();
   }
 })();
