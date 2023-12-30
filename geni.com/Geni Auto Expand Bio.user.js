0a8f596258aa670c00f7c46007263a09b40a4b40
Tommi Rautava <4525639+rautava@users.noreply.github.com>
2023-12-30 21:52:32 +0200

    Update Geni Auto Expand Bio.user.js
    Fixed after geni.com changes

--- geni.com/Geni Auto Expand Bio.user.js
+++ geni.com/Geni Auto Expand Bio.user.js
@@ -1,15 +1,18 @@
 // ==UserScript==
 // @name         Geni Auto Expand Bio
 // @namespace    4525639+rautava@users.noreply.github.com
-// @version      1.2.1
+// @version      1.3
 // @description  Expand Bio sections automatically.
 // @author       Tommi Rautava
 // @license      CC0-1.0
 // @match        https://www.geni.com/people/*
-// @grant        none
 // ==/UserScript==
 
-(function () {
+let LOOP_INTERVAL = 300;
+let MAX_LOOP_COUNT = 20;
+let loopCount = 0;
+
+function clickAnchor() {
   "use strict";
 
   let readMoreElem = document.querySelector('a.toggle_link[id^="bio_"]');
@@ -25,4 +28,22 @@
   while (node = moreChildrenResult.iterateNext()) {
     node.click();
   }
+}
+
+function waitForAnchor() {
+  if (document.querySelector('a.toggle_link[id^="bio_"]')) {
+    clickAnchor();
+  } else {
+    loopCount++;
+
+    if (loopCount <= MAX_LOOP_COUNT) {
+      setTimeout(waitForAnchor, LOOP_INTERVAL);
+    } else {
+      console.error("Timeout");
+    }
+  }
+}
+
+(function () {
+  waitForAnchor();
 })();
