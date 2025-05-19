6399a674da36b2ba83f9e8da24c486832e242677
Tommi Rautava <4525639+rautava@users.noreply.github.com>
2025-05-19 23:06:19 +0300

    Update Geni Swap Street Number.user.js
    Add icon and baptism location.

--- geni.com/Geni Swap Street Number.user.js
+++ geni.com/Geni Swap Street Number.user.js
@@ -1,20 +1,22 @@
 // ==UserScript==
 // @name         Geni Swap Street Number
 // @namespace    4525639+rautava@users.noreply.github.com
-// @version      1.2
+// @version      1.3
 // @description  Swap the street and number in the address fields.
 // @author       Tommi Rautava
 // @license      CC0-1.0
 // @match        https://www.geni.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=geni.com
 // @grant        none
 // ==/UserScript==
 
 let elementIds = [
-  "location_street_address1",
+  "baptism_location_street_address1",
   "birth_location_street_address1",
-  "death_location_street_address1",
   "burial_location_street_address1",
   "current_residence_street_address1",
+  "death_location_street_address1",
+  "location_street_address1",
 ];
 
 let regex = /^(\d+)\s+(.+)\s*$/;
